cf902601fdd010d9a362ebf04d2df6c66c82a5de
Jonathan Golden <jondgolden@me.com>
2026-01-04 10:58:29 -0500

    2026 update

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,7 @@
   -------------------------------------------------------------------------------*/
 
   $(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
+    $('.preloader').fadeOut(1000); // set duration in brackets
   });
 
+document.getElementById("year").innerHTML = new Date().getFullYear();
